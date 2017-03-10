1d30d4e1793eef1ce40b054f736d3560f97e5d65
Shirley Dai <shirleydai423@gmail.com>
2017-03-10 21:33:34 +1300

    add show overlap feature & show value of overlapped area

--- app/rectangle-list/rectangle-list.component.js
+++ app/rectangle-list/rectangle-list.component.js
@@ -21,7 +21,6 @@ component('rectangleList', {
 
 			var overlapValue;
 
-			//TODO drag
 			element.on('mousedown', function(event) {
 				event.preventDefault();
 				startX = event.pageX - x;
@@ -42,20 +41,47 @@ component('rectangleList', {
 			function mouseup() {
 				$document.off('mousemove', mousemove);
 				$document.off('mouseup', mouseup);
-				showOverlap();
+				showOverlap(event);
 			};
 
+			function showOverlap(event) {
+				var elWidth = $(event.target).width();
+				var b1 = $(".rectangle-item")[0],
+					b2 = $(".rectangle-item")[1],
+					x1 = $(b1).position().left,
+					y1 = $(b1).position().top,
+					x2 = $(b2).position().left,
+					y2 = $(b2).position().top;
+				var dx = Math.abs(x1 - x2);
+				var dy = Math.abs(y1 - y2);
 
-
-			//TODO overlap
-			function showOverlap() {
 				var v;
+				var c = $("#operlap");
+
+				if (dx < elWidth && dy < elWidth) {
+					c.css({
+						'width': elWidth - dx,
+						'height': elWidth - dy,
+						'background-color': "green",
+						"position": "absolute",
+						"left": x1 > x2 ? x1 + 1 : x2 + 1,
+						'top': y1 > y2 ? y1 + 1 : y2 + 1
+					});
+
+					v = (elWidth - dx) * (elWidth - dy);
+				} else {
+					v = 0;
+					c.css({
+						'width': 0,
+						'height': 0
+					});
+				}
 
 				setOverlayValue(v);
 			};
 
 			function setOverlayValue(v) {
-
+				$("#operlap-area span").text(v);
 			};
 
 		}
