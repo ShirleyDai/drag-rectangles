1da22bbc741bbab47b3f525934d25062f3c518fe
Shirley Dai <shirleydai423@gmail.com>
2017-03-10 20:26:18 +1300

    setup project

---
+++ app/app.module.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var rectangleApp = angular.module('rectangleApp', []);
+
+rectangleApp.controller('rectangleAppController', function rectangleAppController($scope) {
+	//TODO
+	$scope.test = "1111";
+});
