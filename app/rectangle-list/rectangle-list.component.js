7f41578708765308522d58f6bcf3eff1dc6998fd
Shirley Dai <shirleydai423@gmail.com>
2017-03-10 20:58:31 +1300

    rectangle functions setup

--- app/rectangle-list/rectangle-list.component.js
+++ app/rectangle-list/rectangle-list.component.js
@@ -11,4 +11,34 @@ component('rectangleList', {
 			id: '2'
 		}];
 	}
-});
+}).directive('rectangle', ['$document', function($document) {
+	return {
+		link: function(scope, element, attrs) {
+
+			//TODO drag
+			element.on('mousedown', function() {
+
+			});
+
+			function mousemove(event) {
+
+			};
+
+			function mouseup() {
+
+			};
+
+
+
+			//TODO overlap
+			function showOverlap() {
+
+			};
+
+			function setOverlayValue(v) {
+
+			};
+
+		}
+	}
+}]);
