0021b7f30a338536223b53b45a6a1805c6d61225
Shirley Dai <shirleydai423@gmail.com>
2017-03-10 21:19:09 +1300

    draggable feature

--- app/rectangle-list/rectangle-list.component.js
+++ app/rectangle-list/rectangle-list.component.js
@@ -14,25 +14,44 @@ component('rectangleList', {
 }).directive('rectangle', ['$document', function($document) {
 	return {
 		link: function(scope, element, attrs) {
+			var startX = 0,
+				startY = 0,
+				x = 0,
+				y = 0;
+
+			var overlapValue;
 
 			//TODO drag
-			element.on('mousedown', function() {
-
+			element.on('mousedown', function(event) {
+				event.preventDefault();
+				startX = event.pageX - x;
+				startY = event.pageY - y;
+				$document.on('mousemove', mousemove);
+				$document.on('mouseup', mouseup);
 			});
 
 			function mousemove(event) {
-
+				x = event.pageX - startX;
+				y = event.pageY - startY;
+				$(element).css({
+					top: y + 'px',
+					left: x + 'px'
+				});
 			};
 
 			function mouseup() {
-
+				$document.off('mousemove', mousemove);
+				$document.off('mouseup', mouseup);
+				showOverlap();
 			};
 
 
 
 			//TODO overlap
 			function showOverlap() {
+				var v;
 
+				setOverlayValue(v);
 			};
 
 			function setOverlayValue(v) {
